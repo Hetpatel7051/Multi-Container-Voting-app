3b5825e575859842d1f37ea13ca6d982cdb1527d
Patel Het S <154498049+Hetpatel7051@users.noreply.github.com>
2026-08-17 23:58:55 +0530

    remove unnecessary part of code
    Removed unnecessary part about waiting for DB connection.

--- voting-app/app.py
+++ voting-app/app.py
@@ -5,7 +5,6 @@ import time
 
 app = Flask(__name__)
 
-# Wait for DB to wake up and connect
 def get_db_connection():
     while True:
         try:
@@ -20,7 +19,7 @@ def get_db_connection():
             print("Waiting for database connection...")
             time.sleep(1)
 
-# Initialize Table
+
 conn = get_db_connection()
 cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS votes (id SERIAL PRIMARY KEY, candidate TEXT);")
